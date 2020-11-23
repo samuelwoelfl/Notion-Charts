ea56d6813c168f69e22e1354daefd9af896fcf4b
Samuel Wölfl <samuelwoelfl@gmx.de>
2020-11-23 15:22:41 +0100

    heroku setup

--- notion_charts.py
+++ notion_charts.py
@@ -17,8 +17,23 @@ tableurl = 'https://www.notion.so/samuelwoelfl/fc8d15330f3643338f09633f9219506b?
 # -----------------------------
 # Set up Google Docs Connection
 # -----------------------------
+def create_keyfile_dict():
+    variables_keys = {
+        "type": os.environ.get("SHEET_TYPE"),
+        "project_id": os.environ.get("SHEET_PROJECT_ID"),
+        "private_key_id": os.environ.get("SHEET_PRIVATE_KEY_ID"),
+        "private_key": os.environ.get("SHEET_PRIVATE_KEY"),
+        "client_email": os.environ.get("SHEET_CLIENT_EMAIL"),
+        "client_id": os.environ.get("SHEET_CLIENT_ID"),
+        "auth_uri": os.environ.get("SHEET_AUTH_URI"),
+        "token_uri": os.environ.get("SHEET_TOKEN_URI"),
+        "auth_provider_x509_cert_url": os.environ.get("SHEET_AUTH_PROVIDER_X509_CERT_URL"),
+        "client_x509_cert_url": os.environ.get("SHEET_CLIENT_X509_CERT_URL")
+    }
+    return variables_keys
+
 scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
-creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
+creds = ServiceAccountCredentials.from_json_keyfile_name(create_keyfile_dict(), scope)
 sheets_client = gspread.authorize(creds)
 
 # -----------------------------
